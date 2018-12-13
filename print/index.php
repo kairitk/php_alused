bed6e56e097343871f9a661d0194aa2c5a3f0b2e
käblik <kairit.kivima@khk.ee>
2018-12-13 15:05:02 +0200

    väljastus print ja echo

---
+++ print/index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: käblik
+ * Date: 13.12.2018
+ * Time: 14:49
+ */
+
+print 'See on esimene lause php keeles' . '<br>';
+print 'See on teine lause php keeles' . '<br>';
+// teksti väljastus echo käsuga
+echo 'See tekst on echo abil väljastatud'.'<br>';
+
+
+
+
+
+?>
