e8f1ad855b31e9930c38579109e04c3525aeda86
käblik <kairit.kivima@khk.ee>
2018-12-13 15:28:07 +0200

    muutujate defineerimine ja kutsumine

---
+++ variables/index.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: käblik
+ * Date: 13.12.2018
+ * Time: 15:17
+ */
+
+//muutujate defineerimine
+//$muutujaNimi = väärtus;
+
+//ära kasuta täpitähti muutujate sees, server ei saa nendest aru
+
+$taisArv = 7;
+$reaalArv = 3.14;
+$sone = 'kodu';
+$toevaartusTrue = true;
+$toevaartusFalse = false;
+
+echo '$taisArv'.'<br>';
+echo $taisArv.'<br>';
+echo 'Täisarv = '.$taisArv.'<br>';
+
+echo 'Reaalarv = '.$reaalArv.'<br>';
+echo 'Sõne = '.$sone.'<br>';
+echo 'Tõeväärtus true = '.$toevaartusTrue.'<br>';
+echo 'Tõeväärtus false = '.$toevaartusFalse.'<br>';
+
