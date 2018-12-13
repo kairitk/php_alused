74e888ed8998803f54267d561a61cf2bd5fac34d
käblik <kairit.kivima@khk.ee>
2018-12-13 16:11:45 +0200

    if - else if - else

---
+++ condition/index.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: käblik
+ * Date: 13.12.2018
+ * Time: 15:54
+ */
+
+//tingimuslaused
+// if(tingimus) {
+//      tegevused, mis toimuvad, kui tegevus kehtib
+// } else {
+//      tegevused, mis toimuvad, kui tingimus ei kehti
+//}
+
+
+echo '<h4>Tingimuslaused</h4>';
+
+
+$arv = rand(0,100);  //juhuslik number vahemikus 0-100
+$varv = '';
+
+if ($arv > 0 and $arv < 25) {
+    //antud arv on paaris ja ta on roheline
+    $varv = 'green';
+} else if ($arv > 25 and $arv < 50){
+    //antud arv on paaritu ja ta on punane
+    $varv = 'red';
+} else if ($arv > 50 and $arv < 75){
+    //antud arv on paaritu ja ta on punane
+    $varv = 'yellow';
+} else if ($arv > 75 and $arv < 100){
+    //antud arv on paaritu ja ta on punane
+    $varv = 'brown';
+} else {
+    //antud arv on paaritu ja ta on punane
+    $varv = 'orange';
+}
+
+echo '<p style="color: '.$varv.'">'. $arv . '</p>';
